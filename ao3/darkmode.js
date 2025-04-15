481b71ac1e59fb66c16e4a0338b18303f577b953
kommit <iwanttocommittoasterbath09@gmail.com>
2025-04-15 02:01:52 +0200

    help

---
+++ ao3/darkmode.js
@@ -0,0 +1,11 @@
+function darkmode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+} 
+
+// awesome.
+
+// im gonna yap in here because i dont want to bother him,, i feel like hes still   off, in a way
+// but in a concerned way and "i-dont-want-to-annoy-and/or-tire-him" kinda way instead of "feel alright or else im avoiding you"
+
+// anyways hi!! im rewriting chap1 right now to make it feel more human and consistent and silly
