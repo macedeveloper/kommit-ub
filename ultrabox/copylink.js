90acb3f350d625794eae2d7d9fa17ea894e34148
kommit <iwanttocommittoasterbath09@gmail.com>
2025-08-30 21:41:41 +0200

    Update copylink.js

--- ultrabox/copylink.js
+++ ultrabox/copylink.js
@@ -2,9 +2,8 @@ function copy() {
     navigator.clipboard.writeText(event.target.textContent);
     // thank you so much https://github.com/macedeveloper <3
 
-    var tooltip = document.createElement("span");
-    tooltip.className = "tooltip";
-    tooltip.textContent = "link copied!";
-    event.target.textContent = "";
-    event.target.appendChild(tooltip);
+    const textContent = "successfully copied:\n" + event.target.textContent + "\nto clipboard!";
+    alert(textContent);
+    // alert is bad but man it's the easiest way to do this
+    // the old one is cooler but i scrapped it because it interfered with the copy thing
 }
