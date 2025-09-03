907f85d721292677bbda8d14b27672e1534cac51
Mace Dev <bulava.place@gmail.com>
2025-09-03 18:10:21 +0700

    I think i made it safe to merge now
    - returned back redirects from `page-2,3,4.html`s
    - removed link to unexistent experimental audio.css in baldi.html
    - fixed stupid vscode formating things in generic-sounds.html
    - fixed footer that i broke
    - misc

--- ultrabox/copylink.js
+++ ultrabox/copylink.js
@@ -1,6 +1,7 @@
 function copy() {
     navigator.clipboard.writeText(event.target.textContent);
     // thank you so much https://github.com/macedeveloper <3
+    // no prob :3  ~macedev
 
     const textContent = "successfully copied:\n" + event.target.textContent + "\nto clipboard!";
     alert(textContent);
